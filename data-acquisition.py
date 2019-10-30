6ca0b803a7683752d230b4eab6e137834bc3da62
tigur <thetigur@gmail.com>
2019-10-29 23:27:21 -0600

    Retrieving species data and writing to a file Added said file to gitignore

--- data-acquisition.py
+++ data-acquisition.py
@@ -1,6 +1,9 @@
 ##############
 # Module 8 Homework
 ##############
+import requests
+import json
+
 
 # List the project members
 def the_conspirators():
@@ -13,10 +16,22 @@ def the_conspirators():
 # Function to retrieve data from the API
 def get_data():
     print('Retrieving data here')
+    request = requests.get('http://api.gbif.org/v1/species')
+    json_data = json.loads(request.content)
+
+    # Overwriting the previous file each time this is ran (Not persisting data past a single session)
+    with open(r'species.json', 'w', encoding='utf8') as api_file:
+        json.dump(json_data, api_file)
+
 
 # Function to analyze the retrieved data
 def inspect_data():
     print('Processing data here')
+    with open(r'species.json') as json_file:
+        parsed_json_file = json.load(json_file)
+
+        for item in parsed_json_file:
+            print('item is: ' + item)
 
 
 ################
