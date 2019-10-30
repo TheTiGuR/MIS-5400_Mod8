499c1f1b7db473863542283b3ab03beb87fe41f1
tigur <thetigur@gmail.com>
2019-10-29 22:18:54 -0600

    Added an informational function Implemented main code block to run

--- data-acquisition.py
+++ data-acquisition.py
@@ -1,1 +1,20 @@
-print('Hello World')
+##############
+# Module 8 Homework
+##############
+
+
+def the_conspirators():
+    print('''This project brought to you by: 
+    Chris Simiskey
+    Varsha Mandadi
+    Kristen Sohm    
+    ''')
+
+
+
+################
+# Run all the things
+################
+
+if __name__ == '__main__':
+    the_conspirators()
